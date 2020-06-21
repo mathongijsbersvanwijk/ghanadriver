207dae6ba2adb65a2172c41f461a654d69535605
Mathon Gijsbers van Wijk <zebraverkeer@gmail.com>
2020-06-21 17:22:50 +0200

    corrected unittests

--- questions_per_cat.sql
+++ questions_per_cat.sql
@@ -4,3 +4,5 @@ select d.doc_id, m.value from quagga_doc_meta dm, quagga_metavalue m, quagga_doc
 
 select count(id), category_id from categorizations where categorizable_type = 'App\\Models\\Question' group by category_id order by category_id
 
+SELECT dm.doc_id, mv.value FROM quagga_doc_meta dm, quagga_metavalue mv WHERE dm.id_metavalue = mv.id_metavalue and mv.id_metavar = 10 and doc_id = 319
+
